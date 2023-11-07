7ba98a4b6f58c2d3f5b64d173ce4139f045a9987
Leonardo Coaquira <coaquiraleonardo19@gmail.com>
2023-11-06 19:36:50 -0500

    lab10

---
+++ mi-app/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './formulario/formulario.component';
+
+const routes: Routes = [
+  { path: 'formulario', component: FormularioComponent },
+  { path: '', redirectTo: '/formulario', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
